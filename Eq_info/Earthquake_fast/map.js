270057430b2cfaa071e9110b7287450acd747489
KumahachiBB <kumahachibb@gmail.com>
2021-08-04 22:05:34 +0900

    早い地震情報の震源情報の時の不具合修正

--- Eq_info/Earthquake_fast/map.js
+++ Eq_info/Earthquake_fast/map.js
@@ -52,11 +52,23 @@ const ICON_LIST = {
   },
 };
 
+const P2P_INT_COLOR = {
+  "10": "#99ffff",
+  "20": "#3399ff",
+  "30": "#00ff00",
+  "40": "#ffff33",
+  "45": "#ffaa00",
+  "50": "#ff4000",
+  "55": "#ff0000",
+  "60": "#c41d3b",
+  "70": "#960096",
+};
+
 $(window).on("load", function () {
   map = L.map("map", {
     zoomControl: true,
     minZoom: 4,
-    maxZoom: 10,
+    maxZoom: 13,
     preferCanvas: true,
   });
   map.setView([34.144, 135.527], 5);
@@ -124,7 +136,7 @@ $(window).on("load", function () {
     .done(function (data) {
       ShindoObsPoint = data;
 
-      $.getJSON("https://api.p2pquake.net/v2/history?codes=551")
+      $.getJSON("https://api.p2pquake.net/v2/jma/quake")
         .done(function (data) {
           P2P_quakeget(data[0]);
         });
@@ -146,22 +158,117 @@ let P2PsocketStart = (() => {
     date = new Date();
     console.log(date.toLocaleString("ja"), msg);
 
-    websocket.addEventListener('close', () => console.log('WebSocket closed.'));
+    websocket.addEventListener('close', () => {
+      console.log('WebSocket closed.');
+      setTimeout(P2PsocketStart, 20000);
+    });
   });
 })();
 
 //P2P地震情報プロット
 let marker_list = undefined;
 let jma_int = undefined;
+let fill_layer = undefined;
 
 function P2P_quakeget(msg) {
   if (marker_list !== undefined) {
     map.removeLayer(marker_list);
   }
+  if (fill_layer != undefined) {
+    map.removeLayer(fill_layer);
+  }
+
   console.log(msg);
 
   marker_list = L.featureGroup();
 
+  //震度速報
+  if (msg.issue.type === "ScalePrompt") {
+    //地震メインのJSONと地図データ（塗りつぶし）を読み込む
+    $.getJSON("../assets/japan_quake.json", function (mdata) {
+      let fill_data = [];
+
+      msg.points.forEach((area_data) => {
+        let add_obj = {
+          name: area_data.addr, //String(area_data.addr)でもok(てかそっちのほうがいいかも)
+          int: area_data.scale,
+        };
+        fill_data.push(add_obj);
+      });
+
+      //拡大用に定義
+      let bound = L.latLngBounds();
+
+      //塗りつぶし
+      let fill_geoobje = topojson.feature(mdata, mdata.objects.japan_quake);
+      fill_layer = L.geoJson(fill_geoobje, {
+        onEachFeature: function (feature, layer) {
+          let mapname = feature.properties.name;
+          let code_index = fill_data.findIndex(({ name }) => name === mapname);
+          if (code_index !== -1 && mapname == fill_data[code_index].name) {
+            bound.extend([layer._bounds._northEast, layer._bounds._southWest]);
+
+            let int = undefined;
+            switch (fill_data[code_index].int) {
+              case 10:
+                int = "1";
+                break;
+              case 20:
+                int = "2";
+                break;
+              case 30:
+                int = "3";
+                break;
+              case 40:
+                int = "4";
+                break;
+              case 45:
+                int = "5-";
+                break;
+              case 50:
+                int = "5+";
+                break;
+              case 55:
+                int = "6-";
+                break;
+              case 60:
+                int = "6+";
+                break;
+              case 70:
+                int = "7";
+                break;
+              case 46:
+                int = "震度5弱以上未入電";
+                break;
+            }
+            //ポップアップ設定
+            layer.bindPopup("<p>震度" + int + "： " + fill_data[code_index].name + "</p>");
+          }
+        },
+        style: function (feature) {
+          let fill_color = "transparent";
+          let mapname = feature.properties.name;
+          let code_index = fill_data.findIndex(({ name }) => name === mapname);
+          if (code_index !== -1 && mapname == fill_data[code_index].name) {
+            fill_color = P2P_INT_COLOR[fill_data[code_index].int];
+          }
+
+          return {
+            fillColor: fill_color,
+            fillOpacity: 0.75,
+            opacity: 0.85,
+            weight: 1,
+            color: "#656565",
+            //pane: "fill", //未定義のpaneはエラーの原因 //http://localhost:5500/%E5%9C%B0%E9%9C%87%E6%83%85%E5%A0%B1%E9%96%A2%E9%80%A3/Earthquake_fast/index.html
+          };
+        },
+      }).addTo(map);
+      //塗りつぶし・震源にあわせて拡大
+      map.fitBounds(bound);
+    });
+  }
+
+  //震源アイコン
   if (msg.issue.type === "DetailScale" || msg.issue.type === "Destination") {
     switch (msg.earthquake.maxScale) {
       case 10:
@@ -192,9 +299,9 @@ function P2P_quakeget(msg) {
         max_int = "7";
         break;
     }
-    if(msg.earthquake.hypocenter.depth === 0){
+    if (msg.earthquake.hypocenter.depth === 0) {
       depth_txt = "ごく浅い";
-    }else{
+    } else {
       depth_txt = msg.earthquake.hypocenter.depth + "km";
     }
     let hypo_marker = L.marker([msg.earthquake.hypocenter.latitude, msg.earthquake.hypocenter.longitude],
@@ -208,52 +315,55 @@ function P2P_quakeget(msg) {
     marker_list.addLayer(hypo_marker);
   }
 
-  for (let i = 0; i < msg.points.length; i++) {
-    switch (msg.points[i].scale) {
-      case 10:
-        jma_int = "1";
-        break;
-      case 20:
-        jma_int = "2";
-        break;
-      case 30:
-        jma_int = "3";
-        break;
-      case 40:
-        jma_int = "4";
-        break;
-      case 45:
-        jma_int = "5-";
-        break;
-      case 50:
-        jma_int = "5+";
-        break;
-      case 55:
-        jma_int = "6-";
-        break;
-      case 60:
-        jma_int = "6+";
-        break;
-      case 70:
-        jma_int = "7";
-        break;
-      case 46:
-        jma_int = "震度5弱以上未入電";
-        break;
-    }
-    for (let j = 0; j < ShindoObsPoint.length; j++) {
-      if (ShindoObsPoint[j].name === msg.points[i].addr) {
-        latlon = [ShindoObsPoint[j].lat, ShindoObsPoint[j].lon];
+  if (msg.issue.type === "DetailScale") {
+    //各地の震度アイコン
+    for (let i = 0; i < msg.points.length; i++) {
+      switch (msg.points[i].scale) {
+        case 10:
+          jma_int = "1";
+          break;
+        case 20:
+          jma_int = "2";
+          break;
+        case 30:
+          jma_int = "3";
+          break;
+        case 40:
+          jma_int = "4";
+          break;
+        case 45:
+          jma_int = "5-";
+          break;
+        case 50:
+          jma_int = "5+";
+          break;
+        case 55:
+          jma_int = "6-";
+          break;
+        case 60:
+          jma_int = "6+";
+          break;
+        case 70:
+          jma_int = "7";
+          break;
+        case 46:
+          jma_int = "震度5弱以上未入電";
+          break;
       }
-    }
-    //マーカーを作成。アイコンは最初に追加した部分から引用
-    let add_marker = L.marker(latlon, { pane: "intpane" + jma_int, icon: ICON_LIST[jma_int].icon });
+      for (let j = 0; j < ShindoObsPoint.length; j++) {
+        if (ShindoObsPoint[j].name === msg.points[i].addr) {
+          latlon = [ShindoObsPoint[j].lat, ShindoObsPoint[j].lon];
+        }
+      }
+      //マーカーを作成。アイコンは最初に追加した部分から引用
+      let add_marker = L.marker(latlon, { pane: "intpane" + jma_int, icon: ICON_LIST[jma_int].icon });
 
-    //マーカークリックで表示されるテキストを設定
-    add_marker.bindPopup(`<p>震度${jma_int}：${msg.points[i].addr}</p>`);
-    marker_list.addLayer(add_marker);
-  };
-  marker_list.addTo(map);
+      //マーカークリックで表示されるテキストを設定
+      add_marker.bindPopup(`<p>震度${jma_int}：${msg.points[i].addr}</p>`);
+      marker_list.addLayer(add_marker);
+    };
+    marker_list.addTo(map);
 
-  map.fitBounds(marker_list.getBounds());
+    map.fitBounds(marker_list.getBounds());
+  }
 };
